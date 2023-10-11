b8288141323b2bb144260285909a67c241beceb6
TPoe25 <t_poe25@yahoo.com>
2023-10-10 17:23:49 -0700

    fixing updates

---
+++ models/user.py
@@ -0,0 +1,18 @@
+#!/bin/usr/python3
+""" defines the user class"""
+
+import BaseModel
+
+
+class User(BaseModel):
+    """
+    class for the users email, password, first_name, last_name
+    
+    """
+    
+    
+    def __init__(self):
+        super().__init__(self)
+        
+        User = BaseModel()
+        User.BaseModel()
